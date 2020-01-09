263dd3cbb922352491ea201e6e197c949dbf17fc
DmitryFedyunin <43512329+DmitryFedyunin@users.noreply.github.com>
2020-01-09 19:14:46 +0700

    refactor func
    add array function and refactor

--- src/function/array.js
+++ src/function/array.js
@@ -1,59 +1,66 @@
-export const initCompact = () => {
-    let arrCompact = [2, 0, 6, 7, NaN, 8, false, undefined];
-    let arrPositive = arrCompact.filter(number => {
-        return number !== 0 && number !== false && number !== undefined && number !== '' && number !== null && isNaN(number) !== true;
-    });
+export const initCompact = (arrCompact) => {
+  let arrPositive = arrCompact.filter(number => {
+    return number !== 0 && number !== false && number !== undefined && number !== '' && number !== null && isNaN(number) !== true;
+  });
     document.getElementById('outCompact').innerHTML = arrPositive;
 };
 
-export const initConcat = () => {
-    let arrConcat = [2, 3, 5, 6];
-    arrConcat.unshift(76);
+export const initConcat = (arrConcat) => {
+  arrConcat.unshift(76);
     document.getElementById('outConcat').innerHTML = arrConcat;
 };
 
-export const initDifference = () => {
-  let arrDiff = [1,2,3,4,5,10];
-  let arrDiff2 = [2,4,5];
-  arrDiff = arrDiff.filter(e => !~arrDiff2.indexOf(e))
+export const initDifference = (arrDiff, arrDiff2) => {
+  arrDiff = arrDiff.filter(e => !~arrDiff2.indexOf(e));
   document.getElementById('outDiff').innerHTML = arrDiff;
 };
 
-export const initDrop = () => {
-    let arrDrop = [1,2,3];
-    let arrnew = arrDrop.splice(1,1);
-    document.getElementById('outDrop').innerHTML = arrnew;
+export const initDrop = (arrDrop) => {
+  arrDrop.splice(1,1);
+    document.getElementById('outDrop').innerHTML = arrDrop;
 };
 
-export const initHead = () => {
-  let arrHead = [1,2,3];
-  let newArr = arrHead.shift();
-  document.getElementById('outHead').innerHTML = newArr;
+export const initHead = (arrHead) => {
+  arrHead.shift();
+  document.getElementById('outHead').innerHTML = arrHead;
 };
 
-export const initFlatten = () => {
-  let arrFlat = [1, [2, [3, [4]], 5]];
-  let newFlat = arrFlat.flat();
-  document.getElementById('outFlat').innerHTML = newFlat;
-  console.log(newFlat);
+export const initFlatten = (arrFlat) => {
+  arrFlat.flat();
+  document.getElementById('outFlat').innerHTML = arrFlat;
 };
 
-export const initFlattenDeep = () => {
-  let arrFlatten = [1,2,3,[1,2,3,4, [2,3,4]]];
-  let flattenDeep = arrFlatten.flat(Infinity);
-  document.getElementById('outFlatten').innerHTML = flattenDeep;
-  console.log(flattenDeep);
+export const initFlattenDeep = (arrFlatten) => {
+  arrFlatten.flat(Infinity);
+  document.getElementById('outFlatten').innerHTML = arrFlatten;
 };
 
-export const initInitial = () => {
-  let initArr = [1,2,3];
+export const initInitial = (initArr) => {
   initArr.pop();
   document.getElementById('outInitial').innerHTML = initArr;
 };
 
-export const initIntersection = () => {
-    let arrInterseption = [1,2,3,4,5,10];
-    let arrInterseption2 = [1,2,3,6];
-    arrInterseption = arrInterseption.filter(e => ~arrInterseption2.indexOf(e))
-    document.getElementById('outInter').innerHTML = arrInterseption;
+export const initIntersection = (arr1, arr2) => {
+  arr1 = arr1.filter(e => ~arr2.indexOf(e));
+    document.getElementById('outInter').innerHTML = arr1;
+};
+
+export const initJoin = (arrJoin) => {
+  arrJoin = arrJoin.join('~');
+  document.getElementById('outJoin').innerHTML = arrJoin;
+  console.log(arrJoin);
+};
+
+export const initLast = (arrLast) => {
+  arrLast = arrLast.pop();
+  document.getElementById('outLast').innerHTML = arrLast;
+};
+
+export const initNth = (arrNth) => {
+  arrNth = arrNth.splice(1,1);
+  document.getElementById('outNth').innerHTML = arrNth;
+};
+
+export const iniPull = (arrPull) => {
+  // arrPull = arrPull.
 };
--- src/function/math.js
+++ src/function/math.js
@@ -1,78 +1,87 @@
-export function initAdd() {
-    let num1 = parseInt(document.getElementById('a').value);
-    let num2 = parseInt(document.getElementById('b').value);
-    if (isNaN(num1)===true) num1=0;
-    if (isNaN(num2)===true) num2=0;
+export function initAdd(num1,num2) {
     let result = num1 + num2;
-    document.getElementById('out').innerHTML = result;
+    // return result;
+    console.log(result);
+    // let num1 = parseInt(document.getElementById('a').value);
+    // let num2 = parseInt(document.getElementById('b').value);
+    // if (isNaN(num1)===true) num1=0;
+    // if (isNaN(num2)===true) num2=0;
+    // let result = num1 + num2;
+    return document.getElementById('out').innerHTML = result;
 }
 
-export function initCeil() {
-    let num1 = parseInt(document.getElementById('ceilNum').value);
-    if (isNaN(num1) === true) num1 = 0;
+export function initCeil(num1) {
     let resultCeil = Math.ceil(num1);
+    // let num1 = parseInt(document.getElementById('ceilNum').value);
+    // if (isNaN(num1) === true) num1 = 0;
+    // let resultCeil = Math.ceil(num1);
     document.getElementById('outCeil').innerHTML = resultCeil;
 }
 
-export function initDivide() {
-    let divideNum1 = parseInt(document.getElementById('divideNum1').value);
-    let divideNum2 = parseInt(document.getElementById('divideNum2').value);
-    if (isNaN(divideNum1)===true) divideNum1=0;
-    if (isNaN(divideNum2)===true) divideNum2=0;
-    let resultDivide = divideNum1 / divideNum2;
+export function initDivide(num1,num2) {
+    let resultDivide = num1 / num2;
+    // let divideNum1 = parseInt(document.getElementById('divideNum1').value);
+    // let divideNum2 = parseInt(document.getElementById('divideNum2').value);
+    // if (isNaN(divideNum1)===true) divideNum1=0;
+    // if (isNaN(divideNum2)===true) divideNum2=0;
+    // let resultDivide = divideNum1 / divideNum2;
     document.getElementById('outDivide').innerHTML = resultDivide;
 }
 
-export function initFloor() {
-    let floorNum1 = parseInt(document.getElementById('floorNum1').value);
-    if (isNaN(floorNum1) === true) floorNum1 = 0;
-    let resultFloor = Math.floor(floorNum1);
+export function initFloor(num1) {
+    let resultFloor = Math.floor(num1);
+    // let floorNum1 = parseInt(document.getElementById('floorNum1').value);
+    // if (isNaN(floorNum1) === true) floorNum1 = 0;
+    // let resultFloor = Math.floor(floorNum1);
     document.getElementById('outFloor').innerHTML = resultFloor;
 }
 
-export function initMax() {
-    let maxArray = [4, 2, 8, 6];
-    let resultMax = Math.max(...maxArray);
+export function initMax(someArray) {
+    let resultMax = Math.max(...someArray);
+    // let maxArray = [4, 2, 8, 6];
+    // let resultMax = Math.max(...maxArray);
     document.getElementById('outMax').innerHTML = resultMax;
 }
 
-export function initMean() {
-    let arrMean = [4, 2, 8, 6];
-    let totalMean = 0;
-    for (let i = 0; i < arrMean.length; i++){
-        totalMean += arrMean[i]
+export function initMean(arrMean, totalMean) {
+    for (let i=0;i<arrMean.length;i++ ){
+        totalMean +=arrMean[i]
     }
     let avgMean = totalMean / arrMean.length;
+    // let arrMean = [4, 2, 8, 6];
+    // let totalMean = 0;
+    // for (let i = 0; i < arrMean.length; i++){
+    //     totalMean += arrMean[i]
+    // }
+    // let avgMean = totalMean / arrMean.length;
     document.getElementById('outMean').innerHTML = avgMean;
 }
 
-export function initMin() {
-    let minArray = [4, 2, 8, 6];
-    let resultMin = Math.min(...minArray);
+export function initMin(someArray) {
+    let resultMin = Math.min(...someArray);
+    // let minArray = [4, 2, 8, 6];
+    // let resultMin = Math.min(...minArray);
     document.getElementById('outMin').innerHTML = resultMin;
 }
 
-export function initMultiply() {
-    let multiplyNum1 = parseInt(document.getElementById('multiply1').value);
-    let multiplyNum2 = parseInt(document.getElementById('multiply2').value);
-    if (isNaN(multiplyNum1) === true) multiplyNum1 = 0;
-    if (isNaN(multiplyNum2) === true) multiplyNum2 = 0;
-    let multiplyResult = multiplyNum1 * multiplyNum2;
+export function initMultiply(num1,num2) {
+    let multiplyResult = num1 * num2;
+    // let multiplyNum1 = parseInt(document.getElementById('multiply1').value);
+    // let multiplyNum2 = parseInt(document.getElementById('multiply2').value);
+    // if (isNaN(multiplyNum1) === true) multiplyNum1 = 0;
+    // if (isNaN(multiplyNum2) === true) multiplyNum2 = 0;
+    // let multiplyResult = multiplyNum1 * multiplyNum2;
     document.getElementById('outMultiply').innerHTML = multiplyResult;
 }
 
-export function initRound() {
-    let roundNum1 = parseInt(document.getElementById('roundNum1').value);
-    if (isNaN(roundNum1) === true) roundNum1 = 0;
-    let resultRound = Math.round(roundNum1);
+export function initRound(num1) {
+    let resultRound = Math.round(num1);
     document.getElementById('outRound').innerHTML = resultRound;
 }
 
-export function initSum() {
-    let arrSum = [4, 2, 8, 6];
-    let totalSum = 0;
-    for (let i = 0; i < arrSum.length; i++) {
-        totalSum += arrSum[i]
+export function initSum(someArray, totalSum=0) {
+    for (let i=0; i<someArray.length; i++ ) {
+        totalSum += someArray[i]
     }
     document.getElementById('outSum').innerHTML = totalSum;
 }
